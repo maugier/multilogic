122ce125d553a9d4f88589bec3b452c2634f51bb
Maxime Augier <max@xolus.net>
2022-10-31 21:42:42 +0100

    First test works!

--- src/kdoku.rs
+++ src/kdoku.rs
@@ -1,31 +1,58 @@
-use varisat::{CnfFormula, ExtendFormula, Var, Lit};
+use varisat::{CnfFormula, ExtendFormula, Var, Lit, Solver, solver::SolverError};
 use itertools::Itertools;
+use thiserror::Error;
 
 macro_rules! ary {
     ($f:expr ; $size:literal) => { [(); $size].map(|_| $f) };
 }
 
+#[derive(Clone,Copy,Debug)]
 pub struct U6(u8);
 
-pub type Solution = [[U6; 6]; 6];
+#[derive(Clone,Copy,Debug)]
+pub struct Solution([[U6; 6]; 6]);
 
+#[derive(Clone, Copy, Debug)]
 pub enum Op { Plus, Minus, Times, Div }
 
+#[derive(Debug, Error)]
 pub enum LogicalError<'e> {
+    #[error("Impossible constraint")]
     ImpossibleConstraint(&'e Constraint),
+
+    #[error("Unsupported constraint")]
     UnsupportedConstraint(&'e Constraint),
+
+    #[error("Unsatisfyable")]
+    Unsatisfyable,
+    
+    #[error("SAT Solver error")]
+    SolverError(#[from] SolverError),
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct BaseGrid {
     formula: CnfFormula,
     vars: [[[Var; 6]; 6]; 6],
 }
 
+#[derive(Debug)]
 pub struct Constraint {
     pub op: Op,
     pub result: u8,
-    pub cells: Vec<(U6, U6)>
+    pub cells: Vec<(usize, usize)>
+}
+
+impl std::fmt::Display for Solution {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for line in &self.0 {
+            for cell in line {
+                write!(f, "{}", cell.0)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
 }
 
 impl BaseGrid {
@@ -78,13 +105,31 @@ impl BaseGrid {
     pub fn solve(mut self, constraints: &[Constraint]) -> Result<Solution, LogicalError> {
         for c in constraints { self.add_constraint(c)? };
 
-        todo!()
+        let mut solver = Solver::new();
+        solver.add_formula(&self.formula);
+        solver.solve()?;
+
+        let mut solution = [[U6(0); 6]; 6];
+
+        let model = solver.model().ok_or(LogicalError::Unsatisfyable)?;
+
+        for x in 0..6 {
+            for y in 0..6 {
+                for v in 0..6 {
+                    if model.contains(&self.vars[x][y][v].lit(true)) {
+                        solution[x][y] = U6(v as u8 + 1)
+                    }
+                }
+            }
+        }
+
+        Ok(Solution(solution))
 
     }
 
     fn add_constraint<'c>(&mut self, constraint: &'c Constraint) -> Result<(), LogicalError<'c>> {
         
-        let vars: Vec<_> = constraint.cells.iter().map(|(x,y)| self.vars[x.0 as usize][y.0 as usize]).collect();
+        let vars: Vec<_> = constraint.cells.iter().map(|(x,y)| self.vars[*x][*y]).collect();
 
         let terms = match constraint.op {
             Op::Plus => make_associative_constraint(&vars[..], |a,b| a+b, 0, constraint.result as u16),
@@ -168,5 +213,33 @@ fn make_associative_constraint(vars: &[[Var; 6]], op: fn(u16,u16) -> u16, z: u16
     if terms.is_empty() { return None }
 
     Some(terms)
+
+}
+
+
+
+#[test]
+fn test_sample_grid() {
+
+    let constraints = vec![
+        Constraint { op: Op::Plus,  result: 10, cells: vec![ (0,0), (1,0) ] },
+        Constraint { op: Op::Plus,  result: 11, cells: vec![ (2,0), (3,0), (4,0), (5,0)]},
+        Constraint { op: Op::Plus,  result:  7, cells: vec![ (0,1), (0,2) ]},
+        Constraint { op: Op::Plus,  result: 18, cells: vec![ (1,1), (1,2), (2,1), (3,1)]},
+        Constraint { op: Op::Plus,  result:  6, cells: vec![ (4,1), (4,2), (4,3) ]},
+        Constraint { op: Op::Plus,  result:  7, cells: vec![ (5,1), (5,2)]},
+        Constraint { op: Op::Times, result: 30, cells: vec![ (0,3), (1,3), (2,2), (2,3)]},
+        Constraint { op: Op::Plus,  result:  8, cells: vec![ (3,2), (3,3)]},
+        Constraint { op: Op::Times, result: 24, cells: vec![ (5,3), (5,4)]},
+        Constraint { op: Op::Div,   result:  2, cells: vec![ (0,4), (0,5)]},
+        Constraint { op: Op::Plus,  result:  2, cells: vec![ (1,4)]},
+        Constraint { op: Op::Plus,  result: 13, cells: vec![ (1,5), (2,4), (2,5), (3,5)]},
+        Constraint { op: Op::Minus, result:  1, cells: vec![ (3,4), (4,4)]},
+        Constraint { op: Op::Minus, result:  3, cells: vec![ (4,5), (5,5)]},
+
+    ];
+
+    let solution = BaseGrid::new().solve(&constraints[..]).unwrap();
+    eprintln!("{}", solution);
 
 }
