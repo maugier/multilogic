00fce55bd36908a34fa47700e0c66848d13382c5
Maxime Augier <max@xolus.net>
2023-11-17 12:29:55 +0100

    Combinatorics in utilities

--- src/solve/util.rs
+++ src/solve/util.rs
@@ -71,3 +71,29 @@ impl <'a, T> IndexMut<usize> for MView<'a, T> {
         &mut self.vec[index * self.stride..][..self.stride]
     }
 }
+
+
+pub fn choose(n: usize, k: usize, mut f: impl FnMut(&[bool])) {
+    let mut acc = Vec::with_capacity(n);
+    choose_acc(&mut acc, n, k, &mut f);
+}
+
+fn choose_acc<F>(acc: &mut Vec<bool>, n: usize, k: usize, f: &mut F)
+    where F: FnMut(&[bool])
+{
+    if n == 0 {
+        f(&acc[..]); return
+    }
+
+    if k > 0 {
+        acc.push(true);
+        choose_acc(acc, n-1, k-1, f);
+        acc.pop();
+    }
+
+    if k < n {
+        acc.push(false);
+        choose_acc(acc, n-1, k, f);
+        acc.pop();
+    }
+}
